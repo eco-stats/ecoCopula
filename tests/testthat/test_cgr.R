1c8b5f26485751af3e57164a02de9a78addfcca9
David Warton <David.Warton@unsw.edu.au>
2025-11-24 12:05:57 +1100

    1.0.5
    delete tests and change citEntry for CRAN checks

--- tests/testthat/test_cgr.R
+++
@@ -1,9 +0,0 @@
-test_that("testing cgr so it error", {
-  expect_error(cgr(4))
-})
-
-test_that("obj file type", {
-  spider.mod=manyglm(mvabund(spider$abund)~.,data=as.data.frame(spider$x))
-  expect_s3_class(cgr(spider.mod), "cgr")
-
-  })
